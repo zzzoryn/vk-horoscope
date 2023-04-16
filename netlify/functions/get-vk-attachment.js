b66bd6af91b1e3152452d73b52a7d841d9909950
Aleksandr Zorin <aleksandr-zoryn@ya.ru>
2023-04-16 18:50:47 +0300

    new function

---
+++ netlify/functions/get-vk-attachment.js
@@ -0,0 +1,43 @@
+const {VK} = require('vk-io');
+const {TYPES} = require('../../src/constants');
+
+exports.handler = async function(event) {
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 501,
+      body: JSON.stringify({message: 'Not Implemented'}),
+      headers: {'content-type': 'application/json'}
+    };
+  }
+
+  if (!event.body.image) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({message: 'No image'}),
+      headers: {'content-type': 'application/json'}
+    };
+  }
+
+  try {
+    const vk = new VK({token: process.env.VK_API_TOKEN});
+    const horoscope = TYPES[0];
+
+    const attachment = await vk.upload.wallPhoto({
+      group_id: horoscope.groupId,
+      source: {value: event.body.image}
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({attachment: `photo${attachment.ownerId}_${attachment.id}`}),
+      headers: {'content-type': 'application/json'}
+    };
+  }
+  catch (err) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({type: 'error', message: err.message}),
+      headers: {'content-type': 'application/json'}
+    };
+  }
+};
